da22fb2395c2e643f7565542da2ed351b69fc5ab
Amankumar-18 <amankumarsingh18114914@gmail.com>
2026-07-11 21:44:36 +0530

    add new cors

--- backend/Sisoguide/Sisoguide/settings.py
+++ backend/Sisoguide/Sisoguide/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://my-siso-guide.vercel.app/",
     "https://my-siso-guide-i70ka8tk5-amankumar-18s-projects.vercel.app",
 ]
 
